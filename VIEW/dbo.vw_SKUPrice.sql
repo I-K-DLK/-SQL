afcfc58069ed7ea723edc802be864e828f8fad51
I-K-DLK <107864230+I-K-DLK@users.noreply.github.com>
2024-03-18 22:12:22 +0100

    Update dbo.vw_SKUPrice.sql

--- VIEW/dbo.vw_SKUPrice.sql
+++ VIEW/dbo.vw_SKUPrice.sql
@@ -1,1 +1,7 @@
-
+/*4. Создать представление (на выходе: файл в репозитории dbo.vw_SKUPrice в папке VIEW)
+   4.1 Возвращает все атрибуты продуктов из таблицы dbo.SKU и расчетный атрибут со стоимостью одного продукта 
+   (используя функцию dbo.udf_GetSKUPrice)
+*/
+create or alter view dbo.vw_SKUPrice
+as
+select  ID, Name, dbo.udf_GetSKUPrice(ID) as Price from SKU
