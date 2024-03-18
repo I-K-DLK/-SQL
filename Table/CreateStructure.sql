09b68ead06985bb682b9e2f0167468fb0fb444f9
I-K-DLK <107864230+I-K-DLK@users.noreply.github.com>
2024-03-18 22:34:02 +0100

    Update CreateStructure.sql

--- Table/CreateStructure.sql
+++ Table/CreateStructure.sql
@@ -1,11 +1,12 @@
 /*
-dbo.SKU (ID identity, Code, Name)
-      2.1.1 Ограничение на уникальность поля Code
-      2.1.2 Поле Code вычисляемое: "s" + ID
-   2.2 dbo.Family (ID identity, SurName, BudgetValue)
-   2.3 dbo.Basket (ID identity, ID_SKU (внешний ключ на таблицу dbo.SKU), ID_Family (Внешний ключ на таблицу dbo.Family) Quantity, Value, PurchaseDate, DiscountValue)
-      2.3.1 Ограничение, что поле Quantity и Value не могут быть меньше 0
-      2.3.2 Добавить значение по умолчанию для поля PurchaseDate: дата добавления записи (текущая дата)
+2. Создать таблицы (на выходе: файл в репозитории CreateStructure.sql в папке Table)
+      2.1 dbo.SKU (ID identity, Code, Name)
+            2.1.1 Ограничение на уникальность поля Code
+            2.1.2 Поле Code вычисляемое: "s" + ID
+      2.2 dbo.Family (ID identity, SurName, BudgetValue)
+      2.3 dbo.Basket (ID identity, ID_SKU (внешний ключ на таблицу dbo.SKU), ID_Family (Внешний ключ на таблицу dbo.Family) Quantity, Value, PurchaseDate, DiscountValue)
+            2.3.1 Ограничение, что поле Quantity и Value не могут быть меньше 0
+            2.3.2 Добавить значение по умолчанию для поля PurchaseDate: дата добавления записи (текущая дата)
 */
 if object_id('dbo.SKU') is null
 begin
@@ -37,7 +38,7 @@ begin
         PurchaseDate datetime not null, 
         DiscountValue int not null     
     );
-    alter table dbo.Basket add constraint FK_Basket_ID_SKU foreign key (FK_Basket_ID_SKU ) references dbo.SKU(ID)
+    alter table dbo.Basket add constraint FK_Basket_ID_SKU foreign key (FK_Basket_ID_SKU) references dbo.SKU(ID)
     alter table dbo.Basket add constraint FK_Basket_ID_Family foreign key (FK_Basket_ID_Family) references dbo.Family(ID)
     alter table dbo.Basket add constraint CK_Basket_QuantityValue check (Quantity >= 0 and Value >= 0)
     alter table dbo.Basket add constraint DF_Basket_PurchaseDate default getdate() for PurchaseDate
