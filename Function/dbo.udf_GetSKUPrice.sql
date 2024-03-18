5ebf88f8cbd83b27723a65be378b21e1a33dbdab
I-K-DLK <107864230+I-K-DLK@users.noreply.github.com>
2024-03-18 22:04:29 +0100

    Create dbo.udf_GetSKUPrice.sql

---
+++ Function/dbo.udf_GetSKUPrice.sql
@@ -0,0 +1,18 @@
+/*
+Создать функцию (на выходе: файл в репозитории dbo.udf_GetSKUPrice.sql в папке Function)
+   3.1 Входной параметр @ID_SKU
+   3.2 Рассчитывает стоимость передаваемого продукта из таблицы dbo.Basket по формуле
+      3.1.1 сумма Value по переданному SKU / сумма Quantity по переданному SKU
+   3.3 На выходе значение типа decimal(18, 2)*/
+create or alter function dbo.udf_GetSKUPrice(
+    @ID_SKU int
+)
+returns decimal(18,2)
+begin
+   declare @Price decimal(18,2)
+   select @Price = sum(Value)/sum(Quantity)
+   from SKU
+   where ID = @ID_SKU
+   group by ID
+   return @Price
+end
