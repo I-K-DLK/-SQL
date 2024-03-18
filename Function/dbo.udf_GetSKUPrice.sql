0d638357596998d3ecc29df44a66c4ae6a5df5b5
I-K-DLK <107864230+I-K-DLK@users.noreply.github.com>
2024-03-18 22:21:15 +0100

    Update dbo.udf_GetSKUPrice.sql

--- Function/dbo.udf_GetSKUPrice.sql
+++ Function/dbo.udf_GetSKUPrice.sql
@@ -3,7 +3,8 @@
    3.1 Входной параметр @ID_SKU
    3.2 Рассчитывает стоимость передаваемого продукта из таблицы dbo.Basket по формуле
       3.1.1 сумма Value по переданному SKU / сумма Quantity по переданному SKU
-   3.3 На выходе значение типа decimal(18, 2)*/
+   3.3 На выходе значение типа decimal(18, 2)
+*/
 create or alter function dbo.udf_GetSKUPrice(
     @ID_SKU int
 )
